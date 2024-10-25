19f8598e75b5a477fd6b67456199b36f1196e1dd
leslie-23 <leslieajayi27@gmail.com>
2024-10-25 18:09:56 +0000

    new adds

--- Program2.cs
+++ Program2.cs
@@ -1,10 +1,39 @@
 // using System;
-// using program001;
+// // using program001;
 
-//     public class Program002
+// public class Program002
+// {
+//     public static void Main(String[] args)
 //     {
-//         public static void basicWriteFunc()
+
+
+//         int joyceSalary = 4000;
+//         int emmaSalary = 5000;
+
+//         bool overtimeJoyce = joyceSalary > 4500;
+//         bool overtimeEmma = emmaSalary > 4500;
+
+//         double bonusJoyce = overtimeJoyce ? (joyceSalary * 0.05) : 0;
+//         double bonusEmma = overtimeEmma ? (emmaSalary * 0.05) : 0;
+
+//         Console.WriteLine($"Joyce's salary: ${joyceSalary}");
+//         Console.WriteLine($"Emma's salary: ${emmaSalary}");
+
+//         Console.WriteLine($"Joyce's overtime: {overtimeJoyce}");
+//         Console.WriteLine($"Emma's overtime: {overtimeEmma}");
+
+//         Console.WriteLine($"Joyce's bonus: ${bonusJoyce}");
+//         Console.WriteLine($"Emma's bonus: ${bonusEmma}");
+//         if (overtimeJoyce)
 //         {
-//             Console.WriteLine("Hello, World!");
+//             Console.WriteLine("Joyce gets a bonus");
 //         }
+//         if (overtimeEmma)
+//         {
+//             Console.WriteLine("Emma gets a bonus");
+//         }
+
+
 //     }
+// }
+
